3f5fe65c86ea856b1e3dee3e407e5d89ea17bbcc
EmilseBilse <bandittenxd@gmail.com>
2024-04-04 18:39:22 +0200

    better login and signup

--- Models/userModel.js
+++ Models/userModel.js
@@ -2,11 +2,12 @@ const mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var usersSchema = new Schema({
-  _id: { type: String },
+  username: { type: String },
   email: { type: String },
   fName: { type: String },
   lName: { type: String },
   password: { type: String },
+  color: { type: String }
 });
 
 module.exports = mongoose.model("users", usersSchema);
